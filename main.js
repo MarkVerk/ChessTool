69eb98330356aaef95cc4c261bfc74615031fdca
MarkVerk <mark.vrkh@gmail.com>
2024-12-23 22:50:26 +0300

    dev 2

--- main.js
+++ main.js
@@ -6,18 +6,26 @@ app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
 
+if (!fs.existsSync('tournaments.json')) {
+    fs.writeFileSync('tournaments.json', JSON.stringify([]));
+}
+
+const tournaments = JSON.parse(fs.readFileSync('tournaments.json'));
+
 app.get('/', (req, res) => {
-    res.render('index', {tournaments: [{name: "Something", id: '1'}, {name: "Something 2", id: '2'}, {name: "Something 3", id: '3'}]});
+    res.render('index', {tournaments: tournaments});
 });
 
 
 app.get('/new_tournament', (req, res) => {
-    res.render('new_tournament');
+    tournaments.push({name: (tournaments.length + 1).toString(), id: new Date().getTime().toString()});
+    fs.writeFileSync('tournaments.json', JSON.stringify(tournaments));
+    res.redirect('/');
 });
 
 
 app.get('/tournament/:id', (req, res) => {
-    
+    res.status(200).send(req.params.id);
 });
 
 
